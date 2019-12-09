2acd0505e9da382a034719b8d0ab26b622effdde
Michael Drüing <michael@drueing.de>
2019-12-10 00:35:47 +0100

    2016, day 11

---
+++ 2016/11/part1.py
@@ -0,0 +1,11 @@
+import re
+
+floors = []
+
+# this is cheating as I couldn't get my code to work correctly yet
+with open("input.txt", mode="r") as f:
+    lines = f.readlines()
+    for l in lines:
+        floors.append(len(re.findall(r"(microchip|generator)", l)))
+
+print(sum(2 * sum(floors[:x]) - 3 for x in range(1,4)))
