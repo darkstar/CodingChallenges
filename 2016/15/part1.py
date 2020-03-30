2e4758428c975fb90407c27a57b82633f3d5d887
Michael Drüing <michael@drueing.de>
2020-03-30 17:29:51 +0200

    2016, day 15

---
+++ 2016/15/part1.py
@@ -0,0 +1,23 @@
+import re
+
+discs = []
+
+with open("input.txt") as f:
+    for l in f.readlines():
+        m = re.match(r'Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+).', l)
+        if m:
+            discs.append({"disc": int(m.group(1)), "size": int(m.group(2)), "pos": int(m.group(3))})
+
+t = 0
+
+while True:
+    res = []
+    for x in discs:
+        # starting at time t, disc X takes x steps to reach the destination
+        res.append((x["pos"] + t + x["disc"]) % x["size"])
+
+    if sum(res) == 0:
+        print(t)
+        break
+
+    t += 1
