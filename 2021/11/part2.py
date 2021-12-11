11f2f74ab93a742a480d35222db10d748e682aa5
Michael Drüing <michael@drueing.de>
2021-12-11 11:26:18 +0100

    2021, day 11

---
+++ 2021/11/part2.py
@@ -0,0 +1,50 @@
+def debug(f):
+  for y in f:
+    print("".join(map(str, y)))
+
+def s2l(s):
+  return list(map(int, list(s)))
+
+def neigh(x, y):
+  n = [ (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) ]
+  return list(filter(lambda p: p[0]>=0 and p[0]<10 and p[1]>=0 and p[1]<10, map(lambda f: (f[0] + x, f[1] + y), n)))
+
+with open("input.txt", mode="r") as f:
+  field = [ x.strip() for x in f.readlines() ]
+
+#field = ["5483143223","2745854711","5264556173","6141336146","6357385478",
+#"4167524645","2176841721","6882881134","4846848554","5283751526"]
+
+field = list(map(lambda f: s2l(f), field))
+
+step = 1
+
+while True:
+  # first, increase all energy levels
+  newfield = [ list(map(lambda n: n + 1, y)) for y in field ]
+
+  # now let the flashing begin
+  flashmap = [ [ 0 ] * 10 for y in range(10) ]
+
+  while True:
+    found = 0
+    for y in range(10):
+      for x in range(10):
+        if newfield[y][x] > 9 and flashmap[y][x] == 0:
+          # found a new flashing octopus
+          flashmap[y][x] = 1
+          newfield[y][x] = 0
+          found = found + 1
+          # increase all adjacent values
+          for n in neigh(x, y):
+            nx, ny = n
+            if flashmap[ny][nx] == 0:
+              newfield[ny][nx] = newfield[ny][nx] + 1
+    if found == 0:
+      break
+  if len([ v for y in flashmap for v in y if v == 0 ]) == 0:
+    break
+  step = step + 1
+  field = newfield
+
+print(step)
