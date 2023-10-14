43c6ca5c9a73d18b08ce8cc22a70794b1526ae27
Michael D <michael@drueing.de>
2023-10-14 21:06:25 +0200

    Update Rosalind template

--- Rosalind/template.py
+++ Rosalind/template.py
@@ -1,3 +1,19 @@
+def readfasta(ifile):
+    dna = {}
+    with open(ifile, mode="r") as f:
+        id = None
+        s = ""
+        for l in f.readlines():
+            if l[0] == ">":
+                if id:
+                    dna[id] = s
+                id = l.strip()[1:]
+                s = ""
+            else:
+                s = s + l.strip()
+        dna[id] = s
+    return len(s), dna
+
 def solution(ifile):
     with open(ifile, mode="r") as f:
         s = f.readline().strip()
